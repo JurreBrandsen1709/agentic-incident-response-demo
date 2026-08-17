b7683a6f6561338f4fdd8aeb0f6bb5dd8307b3a9
Brandsen, Jurre <jurre.brandsen@bngbank.nl>
2026-08-17 22:10:51 +0200

    Add ReconciliationJob entrypoint and demo fixtures

--- app/src/ReconciliationJob/Program.cs
+++ app/src/ReconciliationJob/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+using ReconciliationJob;
+
+if (args.Length < 3)
+{
+    Console.Error.WriteLine("Usage: dotnet run --project app/src/ReconciliationJob -- <fixturePath> <fromUtcIso> <toUtcIso>");
+    return 1;
+}
+
+var fixturePath = args[0];
+var fromUtc = DateTime.Parse(args[1], CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal | DateTimeStyles.AssumeUniversal);
+var toUtc = DateTime.Parse(args[2], CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal | DateTimeStyles.AssumeUniversal);
+
+IRecordStore store = RecordStore.FromFixtureFile(fixturePath);
+var records = store.GetRecordsForWindow(fromUtc, toUtc);
+
+Console.WriteLine($"records_processed: {records.Count}");
+return 0;
